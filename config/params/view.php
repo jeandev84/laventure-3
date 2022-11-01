fc1eeb06f70d97d5b647e787cb5a2f7ef7e87e19
jeandev84 <jeandev84@yahoo.com>
2022-11-01 23:25:46 +0300

    add config files

--- config/params/view.php
+++ config/params/view.php
@@ -1,6 +1,19 @@
 <?php
-
-
 return [
 
+    /*
+     |-----------------------------------------------------------------------------------------------------------
+     |   Here define global default layout
+     |   You must set your view path
+     |   - layout    : Global setting of layout. This layout will be used all other application.
+     |   - extension : twig, blade ... ( default extension tpl.php )
+     |   - cacheDir  : Specify the directory caching application views
+     |   - compress  : Used for compressing content page it optimize rendering content. (boolean: true|false)
+     |------------------------------------------------------------------------------------------------------------
+    */
+    'root'      => 'resources/views',
+    'layout'    => 'layouts/default',
+    'extension' => 'tpl.php', // twig, blade ...
+    'cacheDir'  =>  '/tmp/cache/app/views',
+    'compress'  =>  true
 ];
